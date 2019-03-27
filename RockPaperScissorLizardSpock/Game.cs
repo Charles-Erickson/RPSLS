f2f9e78304533a2f54712da4b7187dc30ec26aa6
Charles Erickson <cse621@gmail.com>
2019-03-27 15:38:14 -0500

    first upload.  adding game action list and starting player class

---
+++ RockPaperScissorLizardSpock/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorLizardSpock
+{
+    class Game
+    {
+
+
+        List<string> GameActions = new List<string>();
+
+        public void AddToGameActions(GameActions)
+        {
+            GameActions.Add("rock");
+            GameActions.Add("paper");
+            GameActions.Add("scissors");
+            GameActions.Add("lizard");
+            GameActions.Add("spock");
+        }
+    }
+}
